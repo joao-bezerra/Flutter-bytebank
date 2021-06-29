a2b969a97b0a79aba317510f3a3d4c6c7904d6ab
João Bezerra Leite <joao.leite@restoque.com.br>
2021-06-29 13:40:28 -0300

    ajustando codigo

---
+++ lib/myApp.dart
@@ -0,0 +1,19 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
